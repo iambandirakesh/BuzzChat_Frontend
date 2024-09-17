ba73e07251299640b06ee3fef14ebc5c29c60d7c
iambandirakesh <brr9384@GMAIL.COM>
2024-09-17 13:02:49 +0530

    logout issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import axios from "axios";
 import { Backend_URL, Backend_URL_WITHOUT_API } from "../constrins/constrains";
@@ -13,18 +13,24 @@ import {
 import Sidebar from "../components/Sidebar";
 import logo from "../assets/main_logo1.png";
 import io from "socket.io-client";
+
 function Home() {
   const user = useSelector((state) => state.user);
-
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const location = useLocation();
+  const [loading, setLoading] = useState(true); // Added loading state
+
   const fetchUserDetails = async () => {
+    setLoading(true); // Start loading
     try {
       const URL = `${Backend_URL}/user-details`;
       const response = await axios(URL, { withCredentials: true });
+
       console.log("Home user details", response);
-      if (!response?.data?.success) {
+
+      // Explicitly check if success is false
+      if (response?.data?.success === false) {
         dispatch(logout());
         navigate("/login");
       } else {
@@ -32,11 +38,18 @@ function Home() {
       }
     } catch (err) {
       console.log(err);
+      // Optional: Redirect to login in case of an error
+      dispatch(logout());
+      navigate("/login");
+    } finally {
+      setLoading(false); // Stop loading after response is handled
     }
   };
+
   useEffect(() => {
     fetchUserDetails();
   }, []);
+
   // socket connection
   useEffect(() => {
     const socketConnection = io(Backend_URL_WITHOUT_API, {
@@ -44,15 +57,25 @@ function Home() {
         token: localStorage.getItem("token"),
       },
     });
+
     socketConnection.on("onlineUser", (data) => {
       dispatch(setOnlineUser(data));
     });
+
     dispatch(setSocketConnection(socketConnection));
+
     return () => {
       socketConnection.disconnect();
     };
   }, []);
+
   const basePath = location.pathname === "/";
+
+  // Show loading spinner or message when fetching user details
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="grid lg:grid-cols-[300px,1fr] h-screen  max-h-screen overflow-hidden">
       <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
